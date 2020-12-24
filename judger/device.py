75fff8b1924a3386bf87dc8e9c619f2d8c327615
dangerggg <780110073@qq.com>
2020-12-24 15:44:39 +0800

    fix a congestion bug

--- judger/device.py
+++ judger/device.py
@@ -8,17 +8,25 @@ class Switch(object):
         self.order = order
         self.fan_out: List[int] = [up_out, down_out]
         self.fan_in: List[List[int]] = [[] for i in range(0, scale)] 
+        self.fan_emitter: List[List[int]] = [[] for i in range(0, scale)]
 
     def new_request(self, src: int, dst: int):
         # 0 for upper pin, 1 for lower pin
         self.fan_in[dst].append(src)
+        self.fan_emitter[src].append(dst)
 
     def clear(self):
         for fan_id in range(0, len(self.fan_in)):
             fan_set = set(self.fan_in[fan_id])
             if len(fan_set) > 1:
-                print("Congestion:", string.ascii_uppercase[self.order], fan_id)
+                print("Switch output congestion: ", string.ascii_uppercase[self.order], fan_id)
             self.fan_in[fan_id].clear()
+        
+        for fan_id in range(0, len(self.fan_emitter)):
+            fan_set = set(self.fan_emitter[fan_id])
+            if len(fan_set) > 1:
+                print("Switch input congestion: ", string.ascii_uppercase[self.order], fan_id)
+            self.fan_emitter[fan_id].clear()
     
     def printer(self):
         print(string.ascii_uppercase[self.order], self.fan_in)
